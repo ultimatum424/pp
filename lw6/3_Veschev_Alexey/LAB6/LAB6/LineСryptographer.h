e3a6f0f6842d18c182215e181bcc8729d61f439a
ultimatum424 <Good20032014>
2017-05-25 21:55:20 +0300

    Refactoring

--- lw6/3_Veschev_Alexey/LAB6/LAB6/LineСryptographer.h
+++ lw6/3_Veschev_Alexey/LAB6/LAB6/LineСryptographer.h
@@ -1,15 +1,16 @@
 #pragma once
 #include "—Message.h"
+#include "ICryptographer.h"
 
-class CLine—ryptographer
+class CLine—ryptographer : public ICryptographer
 {
 public:
 	CLine—ryptographer(std::vector<char> & message, CAlphabet alphabet);
 	~CLine—ryptographer();
 
-	void EncipherCaesar(const int key);
-	void EncipherGamma(const std::string & gamma);
-	std::vector<char> GetCharMessage() const;
+	void EncipherCaesar(const int key) override;
+	void EncipherGamma(const std::string & gamma) override;
+	std::vector<char> GetCharMessage() const override;
 private:
 	—Message m_message;
 };
--- lw6/3_Veschev_Alexey/LAB6/LAB6/ThreadСryptographer.h
+++ lw6/3_Veschev_Alexey/LAB6/LAB6/ThreadСryptographer.h
@@ -1,16 +1,17 @@
 #pragma once
 #include "Alphabet.h"
 #include "—Message.h"
+#include "Line—ryptographer.h"
 
-class CThread—ryptographer
+class CThread—ryptographer : public ICryptographer
 {
 public:
 	CThread—ryptographer(std::vector<char> & message, CAlphabet alphabet);
 	void DivisionMessage(size_t threadNumber, int key);
 	void DivisionMessage(size_t threadNumber, const std::string & gamma);
-	void EncipherCaesar(const int key);
-	void EncipherGamma(const std::string & gamma);
-	std::vector<char> GetCharMessage() const;
+	void EncipherCaesar(const int key) override;
+	void EncipherGamma(const std::string & gamma) override;
+	std::vector<char> GetCharMessage() const override;
 	~CThread—ryptographer();
 private:
 	size_t m_countThreads;
